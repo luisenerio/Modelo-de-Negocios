2888f6aac49b76c290e2947c60cd641439521da3
luisenerio <luiselopeznerio@gmail.com>
2022-05-12 17:50:44 -0500

    Add node class to graphs.py

--- graphs.py
+++ graphs.py
@@ -1,2 +1,10 @@
 
 #We implement the data structures of a graph
+class node(object):
+    def __init__(self,name):
+        """ Assume name is a string"""
+        self.name = name
+    def getName(self):
+        return self.name
+    def __str__(self):
+        return self.name
