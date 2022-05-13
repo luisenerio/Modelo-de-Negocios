a2de4fdee32c3cbf9c4f6186a767e2b8575452f0
luisenerio <luiselopeznerio@gmail.com>
2022-05-13 17:10:13 -0500

    Cambios

--- graphs.py
+++ graphs.py
@@ -8,3 +8,16 @@ class node(object):
         return self.name
     def __str__(self):
         return self.name
+
+class edge(object):
+    def __init__(self,src,dest):
+        """ Src and dest are nodes"""
+        self.src = src
+        self.dest = dest
+    def getSource(self):
+        pass
+    def getDestination(self):
+        pass
+    def __str__(self):
+        return self.src.getName() + '->' + self.dest.getName()
+
